a301d7ab57b03caf976a44a17f8a6d3b29df4bf3
andreidavidbira <andrei-david.bira@student.tuiasi.ro>
2026-04-15 15:53:02 +0300

    am creat modelul document pentru upload si procesare

--- core/documents/models.py
+++ core/documents/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# model pentru documente incarcate
+class Document(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    extracted_text = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Document {self.id} - {self.user.username}"
