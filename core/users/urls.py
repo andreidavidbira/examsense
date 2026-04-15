5d12bf35b0331f5acd0c62817335c0208c7259b9
andreidavidbira <andrei-david.bira@student.tuiasi.ro>
2026-04-15 15:05:11 +0300

    am testat endpoint protejat folosind jwt

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import RegisterView
+from .views import ProtectedView
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('register/', RegisterView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('me/', ProtectedView.as_view()),
 ]
--- core/users/views.py
+++ core/users/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User
 from .serializers import RegisterSerializer
+
 
 # view pentru inregistrare utilizator
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+
+# endpoint protejat (test autentificare)
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": f"Salut {request.user.username}, esti autentificat!"
+        })
